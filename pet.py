e314977327f43b7c6e6aaba903efe580f2d9f967
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-21 13:58:10 +0200

    Added the pets

---
+++ pet.py
@@ -0,0 +1,13 @@
+pet = {"name": "", "type": "", "age": 0, "hunger": 0., "toys": "", }
+
+
+petToys + {"cat": ["cratching post", "toy mouse"], "dog": ["chew bone",
+                                                           "frisbee"], "fish": ["underSea castle", "net", "buried treasure"]}
+
+
+def initPet():
+
+
+petType = ""
+
+petOptions
