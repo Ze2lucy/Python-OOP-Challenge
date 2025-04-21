8ee35f8871c64a8900ab858dac1b69aa50ef3fb0
Ze2-lucy <zethulucysitole@gmail.com>
2025-04-21 14:31:22 +0200

    Added pet toys

--- pet.py
+++ pet.py
@@ -1,13 +1,28 @@
-pet = {"name": "", "type": "", "age": 0, "hunger": 0., "toys": "", }
 
+pet = {"name": "", "type": "", "age": 0, "hunger": 0.0, "toys": ""}
 
-petToys + {"cat": ["cratching post", "toy mouse"], "dog": ["chew bone",
-                                                           "frisbee"], "fish": ["underSea castle", "net", "buried treasure"]}
+petToys = {
+    "cat": ["scratching post", "toy mouse"],
+    "dog": ["chew bone", "frisbee"],
+    "fish": ["underSea castle", "net", "buried treasure"]
+}
 
 
 def initPet():
+    petOptions = list(petToys.keys())
+    petType = ""
+
+    while petType not in petOptions:
+        print("Please input a type of pet from the following options:")
+        for option in petOptions:
+            print(option)
+        petType = input("Please input one of the pets: ").lower()
+
+    pet["type"] = petType
+    pet["toys"] = petToys[petType]
+    print(f"You chose a {petType}. Here are its toys: {pet['toys']}")
+    print(petType)
 
 
-petType = ""
-
-petOptions
+# Call the function
+initPet()
